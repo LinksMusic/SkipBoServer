3d55ae34b524414bae01460c8d8e81886c7ea3f4
René Fabian Ripplinger <reneripp@mail.uni-paderborn.de>
2023-03-04 20:16:45 +0100

    add gamestate

--- src/main/java/com/example/skipboserver/datatypes/Gamestate.java
+++ src/main/java/com/example/skipboserver/datatypes/Gamestate.java
@@ -4,13 +4,23 @@ import com.example.skipboserver.datatypes.enums.Phase;
 import org.json.JSONObject;
 
 import java.util.ArrayList;
+import java.util.Stack;
 
 public class Gamestate {
     private Player currentPlayer;
     private Phase currentPhase;
-    public Gamestate(){
-
+    private boolean isOver;
+    private Card from;
+    private Stack<Card> to;
+    public Gamestate(Player player,Phase phase, boolean isOver,Card from, Stack<Card> to){
+        this.currentPlayer = player;
+        this.currentPhase = phase;
+        this.isOver = isOver;
+        this.from = from;
+        this.to = to;
     }
+
+
 
 
 
